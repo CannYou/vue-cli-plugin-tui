1b2c3d42af46d119243b83d6d5c5a392b21ba41d
youcann <2541817610@qq.com>
2020-05-27 11:00:21 +0800

    提交vue-cli-plugin-tui

---
+++ examples/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <t-button type="primary">按钮</t-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TUI from '../packages'
 
 Vue.config.productionTip = false
+Vue.use(TUI)
 
 new Vue({
   render: h => h(App)
